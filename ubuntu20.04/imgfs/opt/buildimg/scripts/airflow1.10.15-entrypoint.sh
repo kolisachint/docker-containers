f3144a78c9f7f960f64a3f55a710df01379cca8f
Sachin Koli <39264878+kolisachint@users.noreply.github.com>
2021-09-01 21:53:39 +0530

    Update airflow1.10.15-entrypoint.sh

--- ubuntu20.04/imgfs/opt/buildimg/scripts/airflow1.10.15-entrypoint.sh
+++ ubuntu20.04/imgfs/opt/buildimg/scripts/airflow1.10.15-entrypoint.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+#
+# Environment configuration for airflow
 
+airflow db init
+
+airflow users create \
+    --username admin \
+    --firstname Sachin \
+    --lastname Koli \
+    --role Admin \
+    --email kolisachint@gmail.com \
+    --password strongpassword 
+
+airflow scheduler &
+
+airflow webserver
